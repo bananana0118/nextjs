ea6afc39481752f6f53f488888931528fb84361d
YeaJeong <bananana0118@gmail.com>
2023-08-21 00:38:45 +0900

    1강 완

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -1,20 +1,34 @@
 import Hi from "./hi";
 
 export default function Cart() {
+  let cart = ["Tomatoes", "Pasta"];
+  //deduplication 같은요청을 여러번 보내면 하나로 압축해줌
   return (
     <div>
       <h4 className="title">Cart</h4>
-      <Hi />
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
+      <CartItem name={cart[0]}></CartItem>
+      <CartItem name={cart[1]}></CartItem>
+      <Banner content={"롯데카드"}></Banner>
+      <Button color={"blue"}></Button>
+      <Button color={"red"}></Button>
+    </div>
+  );
+}
+
+function Banner(props) {
+  return <h5>{props.content} 결제 행사중</h5>;
+}
+
+function Button(props) {
+  return <button style={{ backgroundColor: props.color }}>버튼</button>;
+}
+
+function CartItem(props) {
+  return (
+    <div className="cart-item">
+      <p>{props.name}</p>
+      <p>$40</p>
+      <p>1개</p>
     </div>
   );
 }
--- src/app/list/page.jsx
+++ src/app/list/page.jsx
@@ -1,7 +1,12 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function List() {
   let products = ["Tomatoes", "Pasta", "Coconut"];
+
+  let [count, setCount] = useState([0, 0, 0]);
+
   return (
     <div>
       <h4 className="title">상품목록</h4>
@@ -10,6 +15,25 @@ export default function List() {
           <div key={i} className="food">
             <Image src={`/food${i}.png`} alt="사진" width={100} height={100} />
             <h4>{product} $40</h4>
+            <span>{count[i]}</span>
+            <button
+              onClick={() => {
+                let copy = [...count];
+                copy[i]++;
+                setCount(copy);
+              }}
+            >
+              +
+            </button>
+            <button
+              onClick={() => {
+                let copy = [...count];
+                copy[i]--;
+                setCount(copy);
+              }}
+            >
+              -
+            </button>
           </div>
         );
       })}
